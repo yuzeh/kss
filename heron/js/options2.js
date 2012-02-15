41b6d848d6a25889addc8c080c8fb7f9b4bdd10f
Dan Huang <delta1080@gmail.com>
2012-02-15 13:31:53 -0800

    Added file download capabilities, and set up the server a bit

--- heron/js/options2.js
+++ heron/js/options2.js
@@ -5,25 +5,42 @@
   var get = Util.get;
 
   // constants
-  // TODO: Set up
-  var SERVER_COMPLETION_URL = '';
+  var COMPLETION_URL = 'https://ai.stanford.edu/~yuze/heron/completion.php';
   
   function notifyServerOfCompletion() {
     if (!get('hasNotifiedServerOfCompletion')) {
-      set('hasNotifiedServerOfCompletion', 1);
-      // TODO: set up
-      $.post(SERVER_COMPLETION_URL,
-        { uid : localStorage['uid'] },
-        function(data, textStatus, jqXhr) { });
+      var dat = { uid : localStorage['uid'], upw : localStorage['upw'] };
+      $.post(COMPLETION_URL, dat, function(data, tS, jqXhr) {
+        console.log(data);
+        if (data != "success\n") { // TODO handle error
+        } else {
+          set('hasNotifiedServerOfCompletion', 1);
+        }
+      });
     }
+  }
+
+  function createTextFileDumpOfData(e) {
+    var allTheData = [];
+    var numEntries = get('logCount');
+    for (var i = 0; i < numEntries; ++i)
+      allTheData.push(JSON.parse(localStorage['log-'+i]));
+
+    // Create file, write to filesystem
+    var bb = new BlobBuilder();
+    bb.append(JSON.stringify(allTheData,null));
+    var blob = bb.getBlob('text/plain');
+    saveAs(blob, 'data.json');
   }
 
   $(function() {
     var progress = Math.floor(get('progress'));
+    $('#task_2').click(createTextFileDumpOfData);
     $('#heron_id').text(localStorage['uid']);
     $('#completion_progress').css('width', '' + progress + '%');
     if (progress == 100) {
       $('#done_msg').css('visibility','');
+      notifyServerOfCompletion();
     }
   });
 })();
